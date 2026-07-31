34f970116493ef36d0549ce4ef2ed23bfd8bc90a
Villmort <57332226+Villmort@users.noreply.github.com>
2026-07-31 15:53:42 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,7 @@
-const CACHE = "informes-adulto-v2";
+const CACHE = "informes-adulto-v3";
 const ASSETS = [
-  "./","./index.html","./manifest.json","./gen-192.png","./gen-512.png","./gen-maskable.png",
-  "./template/mimetype","./template/content.xml","./template/styles.xml","./template/settings.xml",
-  "./template/meta.xml","./template/manifest.rdf","./template/META-INF/manifest.xml","./template/thumbnail.png",
-  "./template/Pictures/10000000000008000000005A7D85DF8C.jpg",
-  "./template/Pictures/10000001000006AD000005D03E0EE38F.png"
+  "./","./index.html","./manifest.json","./template-data.js","./membrete.png",
+  "./gen-192.png","./gen-512.png","./gen-maskable.png","./screenshot-1.png","./screenshot-2.png"
 ];
 self.addEventListener("install", (e) => { e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener("activate", (e) => { e.waitUntil(caches.keys().then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k))))); self.clients.claim(); });
